c844590ee946d0e3ca1302792b47c1e0e87e05a0
郭吉成 <ji.chan@foxmail.com>
2017-07-08 13:00:36 +0800

    init

---
+++ BLEDemo/BLEDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEDemo
+//
+//  Created by Guo.JC on 2017/7/8.
+//  Copyright © 2017年 Guo.JC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLEDemo/BLEDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLEDemo
+//
+//  Created by Guo.JC on 2017/7/8.
+//  Copyright © 2017年 Guo.JC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
